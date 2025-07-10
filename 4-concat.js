b11e37f2ba570632f3adf3fba2f556962f70b00f
mugisha-nar12 <mugishanarcisse1@gmail.com>
2025-07-10 20:05:04 +0200

    script that prints two arguments passed to it, in the following format

---
+++ 4-concat.js
@@ -0,0 +1,16 @@
+const args = process.argv.slice(2);
+function printTwoArguments(arg1, arg2) {
+    if (arg1 && arg2) {
+        console.log(`${arg1} is ${arg2}`);
+    }
+    else if (arg1) {
+        console.log(`${arg1} is `);
+    }
+    else if (arg2) {
+        console.log(` is ${arg2}`);
+    }
+    else {
+        console.log("No argument");
+    }
+}
+printTwoArguments(args[0], args[1]);
