e6acff0174289e4b90d7fc411fbb7f9a3dc4ecf5
mugisha-nar12 <mugishanarcisse1@gmail.com>
2025-07-13 22:58:47 +0200

    concat program

--- 4-concat.js
+++ 4-concat.js
@@ -1,5 +1,9 @@
 const args = process.argv.slice(2);
 
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+console.log(`${firstArg} is ${secondArg}`);
 function printArguments([firstArg, secondArg]) {
     if (!firstArg) {
         console.log(" ");
