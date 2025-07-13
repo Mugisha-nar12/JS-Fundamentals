cad7a9ac2f6e60b9f5c881ed6c3039b5eed56f2a
mugisha-nar12 <mugishanarcisse1@gmail.com>
2025-07-13 23:26:22 +0200

    program that prints x times C is fun

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,31 +1,11 @@
-// const args = process.argv.slice(2);
-
-// function printCIsFun(times) {
-//     const count = parseInt(times, 10);
-//     if (isNaN(count)) {
-//         console.log("Missing number of occurrences");
-//         return;
-//     }
-
-//     let output = "";
-//     for (let i = 0; i < count; i++) {
-//         output += "C is fun";
-//         if (i !== count - 1) output += "\n";
-//     }
-//     console.log(output);
-// }
-
-// printCIsFun(args[0]);
-
-
 const x = parseInt(process.argv[2]);
-let output = "";
 
 if (isNaN(x)) {
   console.log("Missing number of occurrences");
-} else {
+} else if (x > 0) {
+  let output = '';
   for (let i = 0; i < x; i++) {
-    output += "C is fun\n";
+    output += 'C is fun\n';
   }
   console.log(output.trim());
 }
