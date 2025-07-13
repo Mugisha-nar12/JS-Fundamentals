4b0350e2a16edaf3852ea03c41e76731068c10d8
mugisha-nar12 <mugishanarcisse1@gmail.com>
2025-07-13 23:14:28 +0200

    multi languages loop program

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,10 +1,15 @@
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-let i=0;
-while(i< languages.length){
-    console.log(languages[i]);
-    i++;
-}
-// for loop
-// for(let i=0; i< languages.length; i++){
+// const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+// let i=0;
+// while(i< languages.length){
 //     console.log(languages[i]);
+//     i++;
 // }
+
+
+
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let output = "";
+for (const line of lines) {
+  output += line + "\n";
+}
+console.log(output.trim());
